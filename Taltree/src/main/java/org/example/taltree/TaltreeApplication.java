91597dba353665a39c0585b88890ed27a32f3bb1
오송주 <dhthdwn7920@gmail.com>
2024-07-16 18:50:46 +0900

    project setting

---
+++ Taltree/src/main/java/org/example/taltree/TaltreeApplication.java
@@ -0,0 +1,13 @@
+package org.example.taltree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaltreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaltreeApplication.class, args);
+    }
+
+}
---
+++ Taltree/src/test/java/org/example/taltree/TaltreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taltree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaltreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
